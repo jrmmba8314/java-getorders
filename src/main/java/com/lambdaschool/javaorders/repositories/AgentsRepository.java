6eaed0c32cd7e9b09558202ed52570f368a969fe
Lauren Emick <laurenemick6@gmail.com>
2020-08-12 07:48:38 -0700

    agent/customer get working

--- src/main/java/com/lambdaschool/javaorders/repositories/AgentsRepository.java
+++ src/main/java/com/lambdaschool/javaorders/repositories/AgentsRepository.java
@@ -1,8 +1,15 @@
 package com.lambdaschool.javaorders.repositories;
 
 import com.lambdaschool.javaorders.models.Agent;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface AgentsRepository extends CrudRepository<Agent, Long>
 {
+    @Query(value = "SELECT a.agentname, c.custname \n" +
+        "FROM agents a LEFT JOIN customers c \n" +
+        "ON a.agentcode = c.agentcode \n" +
+        "GROUP BY c.custname \n" +
+        "ORDER BY a.agentname ", nativeQuery = true)
+    Agent findByAgentCode(long id);
 }
