3498a01f521fdcaf9c99e1c6cb58009ff9e4ed31
Simone Ballard <simone.a.ballard@gmail.com>
2020-11-04 16:09:57 -0700

    SeedData transferred

---
+++ src/main/java/com/javagetorders/OrdersApplication.java
@@ -0,0 +1,24 @@
+package com.lambdaschool.orders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main class to start the application.
+ */
+@SpringBootApplication
+public class OrdersApplication
+{
+
+    /**
+     * Main method to start the application.
+     *
+     * @param args Not used in this application.
+     */
+    public static void main(String[] args)
+    {
+        SpringApplication.run(OrdersApplication.class,
+            args);
+    }
+
+}
