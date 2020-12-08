e19dd58d37fba9852a5a93beb622ca82a3b33a77
Simone Ballard <simone.a.ballard@gmail.com>
2020-12-08 15:08:33 -0700

    controllers created

---
+++ src/main/java/com/lambdaschool/orders/controllers/OrdersController.java
@@ -0,0 +1,40 @@
+package com.lambdaschool.orders.controllers;
+
+import com.lambdaschool.orders.models.Order;
+import com.lambdaschool.orders.services.OrdersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/orders")
+public class OrdersController
+{
+    @Autowired
+    OrdersService ordersService;
+
+    @GetMapping(value = "/advanceamount")
+    public ResponseEntity<?> getOrdersWithAdvanceAmount()
+    {
+        List<Order> myOrderList = ordersService.findOrdersWithAdvanceAmount();
+        return new ResponseEntity<>(myOrderList,
+            HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/order/{ordernum}",
+        produces = {"application/json"})
+    public ResponseEntity<?> getOrderByNumber(
+        @PathVariable
+            long ordernum)
+    {
+        Order o = ordersService.findOrdersById(ordernum);
+        return new ResponseEntity<>(o,
+            HttpStatus.OK);
+    }
+}
