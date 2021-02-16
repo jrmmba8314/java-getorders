c54494514289d51b52d3890325a94efc57f6f451
Leah Tabush <leahtabush@gmail.com>
2021-02-16 12:58:24 -0800

    initial set-up. pulled in yesterdays  files

---
+++ orders/project/src/main/java/com/lambdaschool/orders/services/CustomerServices.java
@@ -0,0 +1,7 @@
+package com.lambdaschool.orders.services;
+
+import com.lambdaschool.orders.models.Customer;
+
+public interface CustomerServices {
+  public Customer save(Customer customer);
+}
---
+++ orders/project/src/main/java/com/lambdaschool/orders/services/OrderServicesImpl.java
@@ -0,0 +1,21 @@
+package com.lambdaschool.orders.services;
+
+import com.lambdaschool.orders.models.Order;
+import com.lambdaschool.orders.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Transactional
+@Service(value = "orderServices")
+public class OrderServicesImpl implements OrderServices{
+  @Autowired
+  private OrderRepository orderRepository;
+
+  @Transactional
+  @Override
+  public Order save(Order order) {
+    return orderRepository.save(order);
+  }
+}
